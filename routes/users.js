c7b4360e5832e590ea74b06a9ea145d0fba353a8
aseemagarwal123 <aseem7570@gmail.com>
2019-06-04 12:15:44 +0530

    First commit

---
+++ routes/users.js
@@ -0,0 +1,85 @@
+const jwt =require('jsonwebtoken');
+const auth=require('../middleware/auth');
+const config=require('config');
+const _ =require('lodash');
+const bcrypt=require('bcrypt');
+const {User, validate} = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const mail=require('../middleware/mail.js');
+const crypto=require('crypto');
+
+
+
+
+router.get('/', async(req,res)=>{
+
+res.send("welcome and discuss");
+});
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('User already registered.');
+
+
+  user=new User(_.pick(req.body,['name','email','password','companyname','companycontact']));
+  const salt=await bcrypt.genSalt(10);
+   user.password =await bcrypt.hash(user.password,salt);
+
+  await user.save();
+ 
+
+const token=user.generateAuthToken();
+res.header('x-auth-token',token).send(_.pick(user,['_id','name','email']));
+});
+
+
+router.post('/forgotPassword',async (req, res) => {
+    if (req.body.email === '') {
+    res.status(400).send('email required');
+    }
+    let user = await User.findOne({ email: req.body.email });
+    
+
+    if (!user) {
+        console.error('email not in database');
+        res.status(403).send('email not in db');
+    } else {
+    var token = user.generateAuthToken();
+    mail({"to":req.body.email,"subject":"change password","text":`http://localhost:3000/api/users/forgotPassword/${token}`});
+
+}
+
+res.header('x-auth-token',token).send(_.pick(user,['_id','name','email']));
+});
+
+
+router.post('/forgotPassword/:token',async (req, res) => {
+ 
+    const token=req.params.token;
+    const decoded=jwt.verify(token,"mysecret");
+    
+
+    const salt2=await bcrypt.genSalt(10);
+    var user = await User.findByIdAndUpdate(decoded._id,
+    {
+    password :await bcrypt.hash(req.body.password,salt2)
+
+    }, { new: true });
+
+
+    await user.save();
+    res.send(_.pick(user,['_id','name','email']));
+
+});
+
+
+
+
+
+
+
+module.exports=router;  
---
+++ server.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const mail=require('../prototype/middleware/mail');
+// const uuid = require('uuid/v4')
+// const session = require('express-session')
+// const FileStore = require('session-file-store')(session);
+
+const auth = require('./routes/auth');
+const users = require('./routes/users');
+
+const express = require('express');
+
+const app = express();
+
+var url = process.env.DATABASEURL || 'mongodb://localhost/facecount';
+mongoose.connect(url);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    next();
+  });
+  var RateLimit = require('express-rate-limit');
+  // important if behind a proxy to ensure client IP is passed to req.ip
+  app.enable('trust proxy'); 
+   
+  var apiLimiter = new RateLimit({
+    windowMs: 15*60*1000, // 15 minutes
+    max: 100,
+  });
+   
+  // only apply to requests that begin with /user/
+  app.use('/user/', apiLimiter);
+app.use(express.json());
+app.use('/api/users', users,apiLimiter);
+app.use('/api/auth', auth,apiLimiter);
+
+const port = process.env.PORT || 3000;
+const id   = process.env.ID;
+
+
+app.listen(port, id, () => console.log(`Listening on port ${port}...`, process.env.DATABASEURL, process.env.vidly_jwtPrivateKey));
